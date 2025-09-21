0ac34c4bdb715c6c261ec937f55a00f30437f7d0
mayamaya <mayamaya@live.cn>
2025-09-21 14:37:24 +0000

    update server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http = require('http')
 const fs = require('fs')
+const path = require('path')
 
 const server = http.createServer()
 
@@ -8,8 +9,10 @@ server.on('request', (req,res) => {
   let file
 
   if(url === '/' && method === 'GET') {
-    fs.readFile("index.html", "utf8", (err, data) => {
-      console.log(data)
+    fs.readFile(path.join(__dirname, "./index.html"), "utf8", (err, data) => {
+      if(err) {
+        return console.log("read file error", err.message)
+      }
       file = data
     })
   }
