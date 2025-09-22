24e3f417b621c7f99396409b52890c3c8684ad02
zextmayamaya <mayamaya@live.cn>
2025-09-22 08:59:15 +0800

    update server.js

--- server.js
+++ server.js
@@ -8,13 +8,17 @@ server.on('request', (req,res) => {
     const {url,method} = req
 
     if(url === '/' && method === 'GET') {
+        console.log("right access!!!")
         fs.readFile(path.join(__dirname, "./index.html"), "utf8", (err, data) => {
+            console.log("error is",err)
+            console.log("data is",data)
             if(err) {
                 return console.log("read file error!!!", err.message)
             }
             res.writeHeader(200,'Content-Type','text/html;charset=utf-8')
             res.write(data)
         })
+        res.end()
     }
 })
 
