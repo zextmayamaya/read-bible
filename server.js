7391b6f8bb9c7189e56e523a002ccbda7f9295e8
zextmayamaya <mayamaya@live.cn>
2025-09-22 08:43:52 +0800

    update server.js

--- server.js
+++ server.js
@@ -5,20 +5,19 @@ const path = require('path')
 const server = http.createServer()
 
 server.on('request', (req,res) => {
-	const {url,method} = req
+    const {url,method} = req
 
-  if(url === '/' && method === 'GET') {
-    fs.readFile(path.join(__dirname, "./index.html"), "utf8", (err, data) => {
-      if(err) {
-        return console.log("read file error", err.message)
-      }
-			res.writeHeader(200,'Content-Type','text/html;charset=utf-8')
-			res.write(data)
-    })
-  }
-	res.end()
+    if(url === '/' && method === 'GET') {
+        fs.readFile(path.join(__dirname, "./index.html"), "utf8", (err, data) => {
+            if(err) {
+                return console.log("read file error!!!", err.message)
+            }
+            res.writeHeader(200,'Content-Type','text/html;charset=utf-8')
+            res.write(data)
+        })
+    }
 })
 
 server.listen(8080, () => {
-		console.log("server is running 8080")
+    console.log("server is running 8080")
 })
